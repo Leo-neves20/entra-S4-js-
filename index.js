27f80a7de92c55e2b802b5e0480deb43c9bef74d
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-06-24 17:11:56 -0300

    entraga de sexta-feira

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function snapCrackle(maxValue){
+
+    let contador = ""
+
+    for(let numero = 1 ; numero <= maxValue; numero++){
+        
+        if(numero % 2 == 1 && numero % 5 == 0 ){
+
+            console.log("SnapCrackle")
+            
+        }else if(numero % 5 == 0 ){
+            
+            console.log("Crackle")
+
+        }else if(numero % 2 == 1){
+            
+            console.log("Snap")
+
+        }else{
+
+            console.log(numero)
+            
+        }
+
+    }
+
+}
+
+console.log(snapCrackle(12))
+
+
